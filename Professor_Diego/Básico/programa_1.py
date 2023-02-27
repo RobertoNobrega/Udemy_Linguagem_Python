3cacb0ec985876c1ee0ecd2b34a9075b0c9d8e39
Roberto Nóbrega <r_t_4@hotmail.com>
2023-02-27 19:17:40 -0300

    Commit 1

---
+++ Professor_Diego/Básico/programa_1.py
@@ -0,0 +1,11 @@
+# Isso é um comentário de uma única linha em Python.
+# Não há "comentário de mais de uma linha"; ou seja, devemos inserir o símbolo de "jogo da velha", para
+# que possamos adicionar nossos comentários.
+
+#  Python:  Linguagem de Programação. Alto Nível, Dinâmica, Fortemente Tipada, Orientação a Objetos, Indentação define blocos de código.
+
+# Criada em 1991, por Guido van Rossum.
+# Como funciona? | Desenvolve o script, Chama o interpretador python, Python compila e exibe os resultados na tela.
+# OBS: O Python já vem instalado nos Sistemas Linux e MacOS. No Windows, é preciso baixá-lo e instalá-lo.
+
+print ("\t Hello World!") # Vai exibir uma mensagem na tela do computador.
